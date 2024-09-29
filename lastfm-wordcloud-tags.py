94b2816d4110cde6644bed6160e22655e1f5d482
dbeley <6568955+dbeley@users.noreply.github.com>
2024-09-29 20:21:09 +0200

    Extract into function for easier re-use

--- lastfm-wordcloud-tags.py
+++ lastfm-wordcloud-tags.py
@@ -1,3 +1,4 @@
+from io import BytesIO
 import pylast
 import configparser
 import logging
@@ -50,6 +51,58 @@ def custom_color_func(
     return f"rgb({int(color[0] * 255)}, {int(color[1] * 255)}, {int(color[2] * 255)})"
 
 
+def create_lastfm_wordcloud(
+    user, timeframe, artists_count, top_tags_count, forbidden_tags
+):
+    dict_frequencies = {}
+    logger.info("Computing word cloud for %s (timeframe %s).", user, timeframe)
+
+    top_artists = user.get_top_artists(period=timeframe, limit=artists_count)
+    for top_item in tqdm(top_artists, dynamic_ncols=True):
+        top_tags = top_item.item.get_top_tags()
+        top_tags = [
+            top_tag
+            for top_tag in top_tags
+            if top_tag.item.name.lower() not in forbidden_tags
+        ]
+        for top_tag in top_tags[0:top_tags_count]:
+            top_tag_name = top_tag.item.name.lower()
+            if top_tag_name in dict_frequencies:
+                dict_frequencies[top_tag_name] += int(top_item.weight) * int(
+                    top_tag.weight
+                )
+            else:
+                dict_frequencies[top_tag_name] = int(top_item.weight) * int(
+                    top_tag.weight
+                )
+
+    wordcloud = WordCloud(
+        background_color="black",
+        colormap="binary",
+        width=2560,
+        height=1440,
+        prefer_horizontal=1.0,
+    ).generate_from_frequencies(dict_frequencies)
+
+    plt.figure()
+    plt.imshow(
+        wordcloud.recolor(color_func=custom_color_func, random_state=3),
+        interpolation="bilinear",
+    )
+    plt.axis("off")
+    buf = BytesIO()
+    plt.savefig(
+        buf,
+        format="png",
+        dpi=300,
+        bbox_inches="tight",
+        pad_inches=0,
+    )
+    plt.close()
+    buf.seek(0)
+    return buf
+
+
 def main():
     args = parse_args()
     network = lastfmconnect()
@@ -63,51 +116,16 @@ def main():
     forbidden_tags = [line.strip() for line in open("forbidden_tags.txt")]
 
     for user in users:
-        dict_frequencies = {}
-        logger.info("Computing word cloud for %s (timeframe %s).", user, args.timeframe)
-        user = network.get_user(user)
-
-        top_artists = user.get_top_artists(
-            period=args.timeframe, limit=args.artists_count
+        lastfm_user = network.get_user(user)
+        wordcloud = create_lastfm_wordcloud(
+            lastfm_user,
+            args.timeframe,
+            args.artists_count,
+            args.top_tags_count,
+            forbidden_tags,
         )
-        for top_item in tqdm(top_artists, dynamic_ncols=True):
-            top_tags = top_item.item.get_top_tags()
-            top_tags = [
-                top_tag
-                for top_tag in top_tags
-                if top_tag.item.name.lower() not in forbidden_tags
-            ]
-            for top_tag in top_tags[0 : args.top_tags_count]:
-                top_tag_name = top_tag.item.name.lower()
-                if top_tag_name in dict_frequencies:
-                    dict_frequencies[top_tag_name] += int(top_item.weight) * int(
-                        top_tag.weight
-                    )
-                else:
-                    dict_frequencies[top_tag_name] = int(top_item.weight) * int(
-                        top_tag.weight
-                    )
-
-        wordcloud = WordCloud(
-            background_color="black",
-            colormap="binary",
-            width=2560,
-            height=1440,
-            prefer_horizontal=1.0,
-        ).generate_from_frequencies(dict_frequencies)
-
-        plt.figure()
-        plt.imshow(
-            wordcloud.recolor(color_func=custom_color_func, random_state=3),
-            interpolation="bilinear",
-        )
-        plt.axis("off")
-        plt.savefig(
-            f"Exports/{user}_{int(time.time())}_{args.timeframe}.png",
-            dpi=300,
-            bbox_inches="tight",
-            pad_inches=0,
-        )
+        with open(f"Exports/{user}_{int(time.time())}_{args.timeframe}.png", "wb") as f:
+            f.write(wordcloud.getbuffer())
 
     logger.info("Runtime : %.2f seconds" % (time.time() - temps_debut))
 
